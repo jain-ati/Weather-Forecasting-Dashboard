cb45c0aae91b7884363eb4dae9fc4706cb0425fb
jain-ati <anshika.skj@gmail.com>
2025-04-15 23:17:28 +0530

    Initialize API key and setup recent search storage

---
+++ app.js
@@ -0,0 +1,3 @@
+// ====== API Key and Local Storage Initialization ======
+const WeatherApiKey = "a51999e64f394f2db431804269472980";
+let recentSearchCities = JSON.parse(localStorage.getItem("recentSearchCities")) || [];
